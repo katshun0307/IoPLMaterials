bcb16a62e6502242f4f53aa79b1e68d101239672
Shuntaro Katsuda <shunten3@gmail.com>
2018-06-25 15:28:17 +0900

    initial code for typing

--- interpreter/main.ml
+++ interpreter/main.ml
@@ -1,21 +1,24 @@
 open Syntax
 open Eval
+open Typing
 
-let rec read_eval_print env =
+let rec read_eval_print env tyenv =
   print_string "# ";
   flush stdout;
   try
     let decl = Parser.toplevel Lexer.main (Lexing.from_channel stdin) in
+    let ty = ty_decl tyenv decl in
     let (id, newenv, v) = eval_decl env decl in
     Printf.printf "val %s = " id;
+    pp_ty ty;
+    print_string " = ";
     pp_val v;
     print_newline();
-    read_eval_print newenv
-  with
-    _ -> print_string("invalid arg.");
-    print_newline();
-    read_eval_print env;;
-
+    read_eval_print newenv tyenv
+  with e ->
+    let msg = Printexc.to_string e in
+    print_string ("there was an error: " ^ msg ^ "\n");
+    read_eval_print env tyenv;;
 
 let initial_env = 
   Environment.extend "ii" (IntV 2)
@@ -23,4 +26,10 @@ let initial_env =
        (Environment.extend "iv" (IntV 4) 
           (Environment.extend "uso" (BoolV false) Environment.empty)))
 
-let _ = read_eval_print initial_env
+let initial_tyenv = 
+  Environment.extend "ii" TyInt
+    ( Environment.extend "iii" TyInt
+        ( Environment.extend "iv" TyInt
+            ( Environment.extend "uso" TyBool Environment.empty)))
+
+let _ = read_eval_print initial_env initial_tyenv
--- interpreter/typing.ml
+++ interpreter/typing.ml
@@ -1,1 +1,46 @@
+open Syntax
 
+exception Error of string
+
+let err s = raise (Error s)
+
+(* type environment *)
+type tyenv = ty Environment.t
+
+let ty_prim op ty1 ty2 = match op with
+  | Plus -> (match ty1, ty2 with
+      | TyInt, TyInt -> TyInt
+      | _ -> err "argument must be integer: +")
+  | Mult -> (match ty1, ty2 with
+      | TyInt, TyInt -> TyInt
+      | _ -> err "argument must be integer: *")
+  | And -> (match ty1, ty2 with
+      | TyBool, TyBool -> TyBool
+      | _ -> err "argument must be boolean: &&")
+  | Or -> (match ty1, ty2 with
+      | TyBool, TyBool -> TyBool
+      | _ -> err "argument must be boolean: ||")
+  | _ -> err "hahaha^^^^^"
+
+let rec ty_exp tyenv = function
+  | Var x -> (try Environment.lookup x tyenv with 
+        Environment.Not_bound -> err ("Variable not bound: " ^ x))
+  | ILit _ -> TyInt
+  | BLit _ -> TyBool
+  | BinOp (op, exp1, exp2) ->
+    let tyarg1 = ty_exp tyenv exp1 in
+    let tyarg2 = ty_exp tyenv exp2 in
+    ty_prim op tyarg1 tyarg2
+  | IfExp (exp1, exp2, exp3) -> 
+    let tyarg1 = ty_exp tyenv exp1 in
+    let tyarg2 = ty_exp tyenv exp2 in
+    let tyarg3 = ty_exp tyenv exp3 in
+    (match tyarg1 with
+     | TyBool -> if tyarg2 = tyarg3 then tyarg2 else err("both types must be same: if")
+     | _ -> err "condition must be boolean: if")
+  (* | LetExp (id, exp1, exp2) ->   *)
+  | _ -> err "not implemented"
+
+let ty_decl tyenv = function
+  | Exp e -> ty_exp tyenv e
+  | _ -> err "not implemented"
